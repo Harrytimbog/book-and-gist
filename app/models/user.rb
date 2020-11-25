2f88ca455098c9bd992a995fecc4b9fe109e61bd
Timilehin Emmanuel <Harriemannie@gmail.com>
2020-11-25 20:23:55 +0100

    create user, booking and gist session models and also add username column to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :gist_sessions, foreign_key: "host_id", dependent: :destroy
 end
